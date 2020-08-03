05851a5aa386ad1153726ba5d509fba0cf6bc449
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-03 19:21:24 +0300

    Task 3

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 "Use Strict";
 //Task 1
-//const numberOfFilms = prompt('Скільки фільмів ви уже побачили?');
+const numberOfFilms = +prompt('Скільки фільмів ви уже побачили?','');
 
 
 //Task 2
-//const  personalMovieDB = [
- //   count = numberOfFilms,
- //    movies = moviesOff = [],
- //     actors = actorsIt = [],
- //      genres = genresIs = [],
- //       privat = false
-//    ];
+const  personalMovieDB = {
+   count : numberOfFilms,
+    movies : moviesOff = {},
+     actors : actorsIt = {},
+      genres : genresIs = [],
+      privat : false
+   };
 
 //Task 3
 
-const lastOfFilms = confirm('Один із останіх фільмів які ви бачили?');
-      ratingOfFilms = prompt('На скільки ви його оціните?');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
