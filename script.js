4424059e89f91b993f2077c77ae36f31064a775c
Vova <volodiabodnarchyk2000@gmail.com>
2020-08-03 18:45:30 +0300

    Task_2

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-"use Strict";
-const numberOfFilms = prompt('Скільки фільмів ви уже побачили?');
+"Use Strict";
+//Task 1
+//const numberOfFilms = prompt('Скільки фільмів ви уже побачили?');
+
+
+//Task 2
+const  personalMovieDB = [
+    count = numberOfFilms,
+     movies = moviesOff = [],
+      actors = actorsIt = [],
+       genres = genresIs = [],
+        privat = false
+    ];
+//Task 3
